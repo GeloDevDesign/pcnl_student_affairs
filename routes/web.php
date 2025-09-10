4d3586209735c3ebd9c11451dadebfe3a3ca28fb
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-09-10 22:41:29 +0800

    Page Header added

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home', [
+        'pageTitle' => 'Admin - Dashboard',
         'user' =>  [
             'name' => 'Angelo',
             'surname' => 'Serenuela'
