3e0965cdb450ab56c43dc078138639162d903cf8
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-09-20 22:59:50 +0800

    asdfasdf

--- resources/js/components/Search.vue
+++ resources/js/components/Search.vue
@@ -9,20 +9,31 @@ const emit = defineEmits(["query-search"]);
 const searchAction = () => {
     emit("query-search", search.value);
 };
+
+const reset = () => {
+    search.value = "";
+    searchAction();
+};
 </script>
 
 <template>
-    <div class="join flex justify-end w-auto">
+    <div class="flex justify-end w-auto gap-2">
         <input
             v-model="search"
-            class="input join-item rounded-l-full bg-white lg:input-md md:input-md input-sm"
+            class="input join-item bg-white lg:input-md md:input-md input-sm"
             placeholder="Search..."
         />
         <button
             @click="searchAction"
-            class="btn btn-primary lg:btn-md md:btn-md btn-sm join-item rounded-r-full"
+            class="btn btn-primary lg:btn-md md:btn-md btn-sm"
         >
             Search
+        </button>
+        <button
+            @click="reset"
+            class="btn btn-neutral lg:btn-md md:btn-md btn-sm join-item"
+        >
+            Reset
         </button>
     </div>
 </template>
