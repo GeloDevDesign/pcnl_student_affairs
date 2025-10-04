fe41416af466991dbeeeac317894fba4dd7711ed
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-10-04 13:12:15 +0800

    Role crud done

--- resources/js/Pages/ssc-officers/officers.vue
+++ resources/js/Pages/ssc-officers/officers.vue
@@ -5,6 +5,7 @@ import { useForm, usePage } from "@inertiajs/vue3";
 import { router } from "@inertiajs/vue3";
 import Swal from "sweetalert2";
 import InputFields from "../../components/InputFields.vue";
+import EntityAction from "../../components/EntityAction.vue";
 import { useToastAlert } from "../../composables/useToastAlert.js";
 
 const { toastAlert } = useToastAlert();
@@ -198,7 +199,6 @@ function openAddRoleModal() {
 }
 
 function openEditRoleModal(role) {
-    console.log(role)
     selectedRole.value = role;
     editRoleForm.reset();
     editRoleForm.clearErrors();
@@ -206,13 +206,12 @@ function openEditRoleModal(role) {
     editRoleForm.name = role.name;
     editRoleForm.description = role.description;
     editRoleDialog.value.showModal();
-  
 }
 
-async function handleDeleteRole(party) {
+async function handleDeleteRole(role) {
     const { isConfirmed } = await Swal.fire({
         title: "DELETE ROLE",
-        text: `Are you sure you want to delete "${party.name}"?`,
+        text: `Are you sure you want to delete "${role.name}"?`,
         icon: "warning",
         showCancelButton: true,
         confirmButtonText: "Yes, delete it!",
@@ -222,19 +221,19 @@ async function handleDeleteRole(party) {
     if (!isConfirmed) return;
 
     isLoading.value = true;
-    router.delete(`/role/${party.id}`, {
+    router.delete(`/role/${role.id}`, {
         preserveScroll: true,
         onSuccess: () => {
             isLoading.value = false;
             toastAlert(
-                page.props.flash.success || "Party deleted successfully!",
+                page.props.flash.success || "Role deleted successfully!",
                 "success"
             );
         },
         onError: () => {
             isLoading.value = false;
             toastAlert(
-                page.props.errors.deleteParty?.[0] || "Failed to delete party!",
+                page.props.errors.deleteParty?.[0] || "Failed to delete role!",
                 "error"
             );
         },
@@ -665,7 +664,7 @@ function handleAssignCandidate() {
                                     {{ party.name }}
                                 </p>
                                 <p class="text-xs text-gray-500">
-                                    {{ party.num_candidates }} Candidates
+                                    {{ party.slogan }}
                                 </p>
                             </div>
                         </div>
@@ -753,26 +752,10 @@ function handleAssignCandidate() {
                             {{ role.name }}
                         </p>
                         <div>
-                            <button
-                                class="btn btn-primary text-white btn-xs bg-blue-100 border-0 mr-2"
-                                @click="openEditRoleModal(role)"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="size-5 text-primary"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                                    />
-                                </svg>
-                            </button>
-
+                            <EntityAction
+                                @edit="openEditRoleModal(role)"
+                                @delete="handleDeleteRole(role)"
+                            />
                             <button
                                 class="btn btn-primary text-white btn-xs"
                                 @click="assignCandidateDialog()"
@@ -816,45 +799,10 @@ function handleAssignCandidate() {
                                     </p>
                                 </div>
                                 <div class="flex gap-1">
-                                    <button
-                                        @click="
-                                            handleDeleteCandidate(role.name)
-                                        "
-                                        class="btn btn-circle btn-xs btn-error"
-                                    >
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke-width="1.5"
-                                            stroke="currentColor"
-                                            class="size-4 stroke-white"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                            />
-                                        </svg>
-                                    </button>
-                                    <button
-                                        class="btn btn-circle btn-xs btn-primary"
-                                    >
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke-width="1.5"
-                                            stroke="currentColor"
-                                            class="size-4 stroke-white"
-                                        >
-                                            <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
-                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                                            />
-                                        </svg>
-                                    </button>
+                                    <EntityAction
+                                        @edit="handleDeleteCandidate(role)"
+                                        @delete="handleDeleteCandidate(role)"
+                                    />
                                 </div>
                             </div>
                         </div>
